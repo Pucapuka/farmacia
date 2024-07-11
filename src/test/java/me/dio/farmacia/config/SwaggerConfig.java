27e0084d83824732f6730ac486acd85924c18617
Pucapuka <traderstrap1@gmail.com>
2024-07-10 21:16:33 -0300

    corrigido a build.gradle e o swaggerConfig

--- src/test/java/me/dio/farmacia/config/SwaggerConfig.java
+++ src/test/java/me/dio/farmacia/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package me.dio.farmacia.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-	
-	@Bean
+
+    @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
